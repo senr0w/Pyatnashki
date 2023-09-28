6c76ce68e7cce357b903cc4c6b51f38a140491e8
senr0w <129827335+senr0w@users.noreply.github.com>
2023-09-28 09:53:30 +0700

    Add files via upload

--- Pyatnashki/Program.cs
+++ Pyatnashki/Program.cs
@@ -1,88 +1,105 @@
+﻿using System;
 
-
-using System;
-using System.ComponentModel;
-
-int[,] nums = new int[4, 4]{
-    {1,2,3,4},    
-    {5,6,7,8},
-    {9,10,11,12},    
-    {13,14,15,0}
-};
-int w = nums.GetLength(0); int h = nums.GetLength(1);
-Random rand = new Random();
-bool gameOver = false;
-for (int i = w * h - 1; i > 0; i--)
+int[,] nums = new int[4, 4]
 {
-    int line = i / w; int tab = i % h;
-    int line2 = rand.Next(0, w);
-    int tab2 = rand.Next(0, h);
-    int temp = nums[line, tab]; nums[line, tab] = nums[line2, tab2];
-    nums[line2, tab2] = temp;
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 0}
+};
+
+Random random = new Random();
+int w = nums.GetLength(0);
+int h = nums.GetLength(1);
+int n = w * h;
+int moves = 0; // Счётчик ходов
+
+// Перемешивание элементов в матрице
+while (n > 1)
+{
+    n--;
+    int k = random.Next(n + 1);
+    int temp = nums[n / h, n % h];
+    nums[n / h, n % h] = nums[k / h, k % h];
+    nums[k / h, k % h] = temp;
 }
 
-while (!gameOver)
+bool isCorrectOrder = false;
+
+// Основной цикл игры
+while (!isCorrectOrder)
 {
-    Console.Clear();
-    for (int i = 0; i < nums.GetLength(0); i++)
+    // Вывод текущего состояния матрицы
+    for (int i = 0; i < w; i++)
     {
-        for (int j = 0; j < nums.GetLength(1); j++)
+        for (int j = 0; j < h; j++)
         {
             Console.Write(nums[i, j] + "\t");
         }
         Console.WriteLine();
     }
+       //Вывод количества ходов
+     Console.WriteLine("Количество ходов: " + moves);
+    // Пользователь вводит число для замены
+    Console.WriteLine("Число для замены:");    
+     int b = Convert.ToInt32(Console.ReadLine());
+ 
+    int line3 = -1, tab3 = -1;
+    int line4 = -1, tab4 = -1;
 
-    int num = 1;
-    bool win = true; 
-
+    // Поиск позиции пустой ячейки и выбранного числа b
     for (int i = 0; i < w; i++)
     {
         for (int j = 0; j < h; j++)
         {
-            if (nums[i, j] != num)
-            {
-                win = false; break;
-            }
-            num++;
-        }
-        if (!win)
-            break;
-    }  
-    
-
-    int a = 0; Console.WriteLine("Выберите число для заменки");
-    int b = Convert.ToInt32(Console.ReadLine());
-    int line3 = -1, tab3 = -1;
-    int line4 = -1, tab4 = -1; for (int i = 0; i < nums.GetLength(0); i++)
-    {
-        for (int j = 0; j < nums.GetLength(1); j++)
-        {
-            if (nums[i, j] == a)
+            if (nums[i, j] == 0)
             {
                 line3 = i;
                 tab3 = j;
             }
             if (nums[i, j] == b)
             {
-                line4 = i; tab4 = j;
+                line4 = i;
+                tab4 = j;
             }
         }
-    }   
-
-    if (Math.Abs(line3 - line4) + Math.Abs(tab3 - tab4) == 1)
+    }
+    Console.Clear();
+    // Проверка возможности перемещения и, если возможно, перемещение
+    if ((Math.Abs(line3 - line4) == 1 && tab3 == tab4) || (Math.Abs(tab3 - tab4) == 1 && line3 == line4))
     {
         int temp = nums[line3, tab3];
-        nums[line3, tab3] = nums[line4, tab4]; nums[line4, tab4] = temp;
+        nums[line3, tab3] = nums[line4, tab4];
+        nums[line4, tab4] = temp;
+        moves++; // Увеличение счётчика ходов
+       
     }
     else
     {
         Console.WriteLine("Так нельзя");
     }
-    if (win)
+
+    // Проверка правильного порядка чисел в матрице
+    isCorrectOrder = CheckCorrectOrder(nums);
+}
+
+// Вывод сообщения о победе
+Console.WriteLine("Поздравляем! Вы выиграли!");
+
+// Функция для проверки правильного порядка чисел в матрице
+bool CheckCorrectOrder(int[,] array)
+{
+    int value = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.WriteLine("WINNER!!!");
-            gameOver = true;
-            break;
+            if (array[i, j] != value % (w * h))
+            {
+                return false;
+            }
+            value++;
         }
+    }
+    return true;
 }
